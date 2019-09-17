3c77340409ce895e6d246fe3b422371de8568628
gamboama_sd2082@CEACCS.NET <gamboama_sd2082@CEACCS.NET>
2019-09-17 13:28:15 +0800

    Commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,66 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!--<img alt="Vue logo" src="./assets/logo.png">-->
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+
+//import Form from './form/Form.vue'
 
 export default {
-  name: 'app',
+  // data() {
+  //   return {};
+  // },
+  beforeCreate() {
+    //eslint-disable-next-line
+    /*eslint-disable*/
+    console.log("before created");
+  },
+  created() {
+    console.log("created");
+  },
+  beforeMount(){
+    console.log("before mounted")
+  },
+  mounted(){
+    console.log("mounted")
+  },
+  beforeUpdate(){
+    console.log("before updated")
+  },
+  updated(){
+    console.log("updated")
+  },
+  beforeDestroy(){
+    console.log("before destroyed")
+  },
+  destroyed(){
+    console.log("destroyed")
+  },
+  name: "app",
   components: {
     HelloWorld
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  font-family: "Times New roman", Helvetica, Arial, sans-serif;
+  /* -webkit-font-smoothing: antialiased; */
+  /* -moz-osx-font-smoothing: grayscale; */
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: white;
+  font-size: 50px;
+  margin-top: block;
 }
+body{
+        background-image: url(http://www.sclance.com/backgrounds/key-background/key-background_1376694.jpg);
+        background-size: cover;
+        background-repeat: no-repeat;
+    } 
+
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,124 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-secondary">
+      <a class="navbar-brand" href="#">Easakay</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Login</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Register</a>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <input
+            class="form-control mr-sm-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+      </div>
+    </nav>
+    <h1 class="text-primary">Login using VueJs</h1>
+    <div class="row loginPage">
+      <div class="col-md-4"></div>
+      <div class="col-md-4">
+        <div class="container">
+          <div class="card">
+            <div class="card-header">Login</div>
+            <div class="card-body">
+              <div class="form-group">
+                <label for="username" class="bmd-label-floating">Username</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="uname"
+                  placeholder="Enter your username here!"
+                />
+              </div>
+              <div class="form-group">
+                <label for="pwd" class="bmd-label-floating">Password</label>
+                <input
+                  type="password"
+                  class="form-control"
+                  id="passw"
+                  placeholder="Enter your password!"
+                />
+              </div>
+              <center>
+                <button type="button" class="btn btn-outline-success login-btn" id="btnLogin">Login</button>
+                <!-- <input type = "login" class = "form -control" id = "login" value = "Login" v-on:> -->
+              </center>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+ <script>
+//eslint-disable-next-line
+/*eslint-disable*/
+window.$ = require("jquery");
+window.JQuery = require("jquery");
+$(document).ready(function() {
+  $("input").attr("required", true);
+  // $("#email").prop('required', 'required');
+  $("#btnLogin").click(function() {
+    let input = $("#uname").val();
+    let pass = $("#passw").val();
+    $("#passw").attr({
+      pattern: "[A-Za-z]{8,}",
+      title: "This field is required"
+    });
+    if (input != "" && pass != "") {
+      console.log($("#uname").val());
+      console.log($("#passw").val());
+    }
+  });
 
-<script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  // $("#btnSignup").click(function(){
+  //     $("#homepage").hide();
+  //     $("#test").show();
+  // })
+});
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+/* body{
+        background-image: url(http://www.sclance.com/backgrounds/key-background/key-background_1376694.jpg);
+        background-size: cover;
+        background-repeat: no-repeat;
+    } */
+.card {
+  background-color: black;
+  margin-top: 40px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+  
+
+.h3 {
+  margin-left: 500px;
+  margin-top: 300px;
+  font-size: 40px;
+  font-family: Times new;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.nav {
+  margin-block: auto;
 }
 </style>
